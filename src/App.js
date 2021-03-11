77ff9ff74be8bd4e9b217465b143126a0ded11a4
nasras03 <simon.nasrallah@bbc.co.uk>
2021-03-11 19:10:37 +0000

    initial commit, with basic downloading of json using react and menu possibly for nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Navbar extends React.Component{
+    render() {
+        return (
+            <div>
+              <ul id="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+        );
+    }
+}
+
+//function getArticle(url) {
+//   return fetch(url)
+//   .then((response) => response.json())
+//   .then((responseJson) => {
+////     console.log(responseJson.title)
+//     return responseJson;
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+//}
+
+async function getArticle(url) {
+  try {
+    let response = await fetch(url);
+    let responseJson = await response.json();
+//    console.log(responseJson.title)
+    return responseJson;
+   } catch(error) {
+    console.error(error);
+  }
+}
+
+function logger(text) {
+  console.log(text)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ getArticle('https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-1.json')
+   .then(res => logger(res)).catch(err => console.log(err));
+//  const article1 =  getArticle('https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-1.json');
+//  getArticle('https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-2.json');
+//  getArticle('https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-3.json');
+//  console.log(article1)
+
+   return (
+      <div>
+        <Navbar/>
+        <div>
+          [Page content here]
+        </div>
+      </div>
+      );
 }
 
 export default App;
