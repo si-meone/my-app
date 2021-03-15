80f0ac86bdfc8b6c0f04bb4de310444f06c4aa00
nasras03 <simon.nasrallah@bbc.co.uk>
2021-03-15 00:42:25 +0000

    add tests, really basic ones work . commented out a test [when clicking and article that ranking is available] need to figure out how to stub requests or if app can make requests at the moment i just get the home page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, getByText, click} from '@testing-library/react';
 import App from './App';
+import React from 'react'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
+test('renders returns title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/BBC News/i);
+  expect(titleElement).toBeInTheDocument();
 });
+
+test('instructions are loaded on home', () => {
+  render(<App />);
+  const instructions = screen.getByText(/Please choose an article above/i);
+  expect(instructions).toBeInTheDocument();
+});
+
+test('no rank info is loaded initially', () => {
+  render(<App />);
+  const rank = screen.getByText(/rank/i);
+  !expect(rank).toBeInTheDocument();
+});
+
+//TODO find how to stubb the request of the APP component so we can check test the ranking behaviour
+//test('when clicking and article that ranking is available', () => {
+//  render(<App />);
+//  const link = screen.getByRole('link', { name: /Article1/i });
+//  userEvent.click(link);
+//  const link = getByText('/Article1/');
+//  fireEvent.click(link);
+//  const rank = screen.getByText(/Rank article1/i);
+//  expect(rank).toBeInTheDocument();
+//  const rank = screen.getByText(/Rank article1/i);
+//  expect(rank).toBeInTheDocument();
+//  expect(screen.getByRole('button')).toBeInTheDocument();
+//});
